47f7cdcf70890182f125387a91a09cf9d1a22d7b
abubakar200 <sheikhabubakr4@gmail.com>
2024-02-17 11:22:23 +0500

    chnages

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -8,7 +8,7 @@ interface BoardIdPageProps {
   };
 }
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
-  return <Loading />;
+
   return (
     <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
